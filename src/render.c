5275c87bdd6feaec19d0a0b0549db3e2d3fb9e6a
hrushw <192113352+hrushw@users.noreply.github.com>
2025-04-14 03:15:02 +0530

    Mass rename

--- src/render.c
+++ src/render.c
@@ -8,32 +8,32 @@
 #include <stdarg.h>
 #include <math.h>
 
-enum _Sizes { IQSZ_ = 256, CHBUFSZ_ = 1 << 20 };
+enum e_sizes { IQSZ_ = 256, CHBUFSZ_ = 1 << 20 };
 
 /* Keypress struct - don't care about scancode or window */
-struct _glfw_inputevent {
+struct t_glfw_inputevent {
 	int key, action, mods;
 	double mx, my, time;
 };
 
 /* Queue keyboard and mouse input events to be evaluated  */
-struct _glfw_inputqueue {
+struct t_glfw_inputqueue {
 	int start, end;
-	struct _glfw_inputevent queue[IQSZ_];
+	struct t_glfw_inputevent queue[IQSZ_];
 };
 
-struct _glfw_winstate {
+struct t_glfw_winstate {
 	int width, height;
 	/* Mouse x, y position */
 	double mx, my;
 	double time;
 	int runstate;
 	/* Queue of keypresses to evaluate at once */
-	struct _glfw_inputqueue iq;
+	struct t_glfw_inputqueue iq;
 };
 
 /* Window state - global structure */
-struct _glfw_winstate ws = {
+struct t_glfw_winstate ws = {
 	.width = 640, .height = 480,
 	.mx = 0, .my = 0,
 	.time = 0,
@@ -46,30 +46,30 @@ struct _glfw_winstate ws = {
 char g_charbuf[CHBUFSZ_] = {0};
 
 /* Destroy window - wrapper function for atexit */
-void __glfw_window_destroy(void) {
+void f__glfw_window_destroy(void) {
 	glfwDestroyWindow(glfwGetCurrentContext());
 }
 
 /* Reset input queue */
-void _iqclear(struct _glfw_inputqueue *q) {
-	memset(q->queue, 0, IQSZ_ * sizeof(struct _glfw_inputevent));
+void f_iqclear(struct t_glfw_inputqueue *q) {
+	memset(q->queue, 0, IQSZ_ * sizeof(struct t_glfw_inputevent));
 	q->start = 0, q->end = 0;
 }
 
 /* Check if input queue is set properly - else print error message and reset  */
-void _iqcheck(struct _glfw_inputqueue *q) {
+void f_iqcheck(struct t_glfw_inputqueue *q) {
 	/* Bounds check for queue just in case */
 	/* iqstart must be bounded to [0, IQSZ_-1], while iqend must be bounded to [0, 2*IQSZ_-1] */
 	if(! (q->start < 0 || q->start >= IQSZ_ || q->end < 0 || q->end >= 2*IQSZ_ || q->end - q->start >= IQSZ_) )
 		return; 
 
 	fprintf(stderr, "ERROR: Key press queue indices out of bounds!\n(start index = %d, end index = %d, max queue size = %d)\n", q->start, q->end, IQSZ_);
-	_iqclear(q);
+	f_iqclear(q);
 }
 
-void _iqappend(struct _glfw_inputqueue *q, int key, int action, int mods, double mx, double my, double time) {
-	_iqcheck(q);
-	q->queue[q->end] = (struct _glfw_inputevent) {
+void f_iqappend(struct t_glfw_inputqueue *q, int key, int action, int mods, double mx, double my, double time) {
+	f_iqcheck(q);
+	q->queue[q->end] = (struct t_glfw_inputevent) {
 		.key = key, .action = action, .mods = mods,
 		/* Mouse x,y coordinates and time are not rechecked in key callback function */
 		.mx = mx, .my = my, .time = time
@@ -78,21 +78,21 @@ void _iqappend(struct _glfw_inputqueue *q, int key, int action, int mods, double
 }
 
 /* Immediately exit on any error encountered by GLFW */
-void _glfw_callback_error(int err, const char* desc) {
+void f_glfw_callback_error(int err, const char* desc) {
 	fprintf(stderr, "GLFW ERROR: %s\n(Error code - %d)\n", desc, err);
 	exit(EXIT_FAILURE);
 }
 
 /* Key callback: simply add pressed key to queue for evaluation, immediately exit on queue overflow */
 /* Additionally store mouse coordinates into queue */
-void _glfw_callback_key(GLFWwindow *window, int key, int scancode, int action, int mods) {
-	_iqappend(&ws.iq, key, action, mods, ws.mx, ws.my, ws.time);
+void f_glfw_callback_key(GLFWwindow *window, int key, int scancode, int action, int mods) {
+	f_iqappend(&ws.iq, key, action, mods, ws.mx, ws.my, ws.time);
 	/* Window and scancode remain unused */
 	(void)window, (void)scancode;
 }
 
 /* Cursor position callback: simply update global mouse coordinates */
-void _glfw_callback_cursorpos(GLFWwindow *window, double x, double y) {
+void f_glfw_callback_cursorpos(GLFWwindow *window, double x, double y) {
 	ws.mx = x, ws.my = y;
 	/* Window remains unused */
 	(void)window;
@@ -100,23 +100,23 @@ void _glfw_callback_cursorpos(GLFWwindow *window, double x, double y) {
 
 /* Mouse click callback: same as key callback */
 /* (this assumes mouse clicks and keypresses have distinct keycodes) */
-void _glfw_callback_mouseclick(GLFWwindow *window, int button, int action, int mods) {
-	_iqappend(&ws.iq, button, action, mods, ws.mx, ws.my, ws.time);
+void f_glfw_callback_mouseclick(GLFWwindow *window, int button, int action, int mods) {
+	f_iqappend(&ws.iq, button, action, mods, ws.mx, ws.my, ws.time);
 	/* Window remains unused */
 	(void)window;
 }
 
 /* Callback for framebuffer resize events (i.e window resize events) */
-void _glfw_callback_fbresize(GLFWwindow *window, int width, int height) {
+void f_glfw_callback_fbresize(GLFWwindow *window, int width, int height) {
 	ws.width = width, ws.height = height;
 	/* Window remains unused */
 	(void)window;
 }
 
-enum wintype { WIN_DEF, WIN_MAX, WIN_FSCR };
+enum e_wintype { WIN_DEF, WIN_MAX, WIN_FSCR };
 
 /* Create window - optionally maximize and make it fullscreen */
-GLFWwindow* _glfw_crwin(struct _glfw_winstate *wst, const char* title, enum wintype type) {
+GLFWwindow* f_glfw_crwin(struct t_glfw_winstate *wst, const char* title, enum e_wintype type) {
 	GLFWmonitor* mon = glfwGetPrimaryMonitor();
 	if(!mon) exit(EXIT_FAILURE);
 
@@ -145,8 +145,8 @@ GLFWwindow* _glfw_crwin(struct _glfw_winstate *wst, const char* title, enum wint
 }
 
 /* Initialize glfw, create window, set callback functions, initialize OpenGL context, global GLFW settings */
-void _glfw_initialize(struct _glfw_winstate *wst, const char* title) {
-	glfwSetErrorCallback(_glfw_callback_error);
+void f_glfw_initialize(struct t_glfw_winstate *wst, const char* title) {
+	glfwSetErrorCallback(f_glfw_callback_error);
 	glfwInit();
 
 	atexit(glfwTerminate);
@@ -159,13 +159,13 @@ void _glfw_initialize(struct _glfw_winstate *wst, const char* title) {
 
 	glfwWindowHint(GLFW_SAMPLES, GLFW_FALSE);
 
-	GLFWwindow* win = _glfw_crwin(wst, title, WIN_DEF);
-	atexit(__glfw_window_destroy);
+	GLFWwindow* win = f_glfw_crwin(wst, title, WIN_DEF);
+	atexit(f__glfw_window_destroy);
 
-	glfwSetKeyCallback(win, _glfw_callback_key);
-	glfwSetCursorPosCallback(win, _glfw_callback_cursorpos);
-	glfwSetMouseButtonCallback(win, _glfw_callback_mouseclick);
-	glfwSetFramebufferSizeCallback(win, _glfw_callback_fbresize);
+	glfwSetKeyCallback(win, f_glfw_callback_key);
+	glfwSetCursorPosCallback(win, f_glfw_callback_cursorpos);
+	glfwSetMouseButtonCallback(win, f_glfw_callback_mouseclick);
+	glfwSetFramebufferSizeCallback(win, f_glfw_callback_fbresize);
 
 	glfwMakeContextCurrent(win);
 	glfwSwapInterval(1);
@@ -197,7 +197,7 @@ void _io_filetobuf(const char* path, int* len, char* buf, int buflen) {
 }
 
 /* Check if shader was compiled successfully */
-void _gl_chkcmp(unsigned int s, char* infolog, int il_len) {
+void f_gl_chkcmp(unsigned int s, char* infolog, int il_len) {
 	int success = 0;
 	glGetShaderiv(s, GL_COMPILE_STATUS, &success);
 	if(success) return;
@@ -212,19 +212,19 @@ void _gl_chkcmp(unsigned int s, char* infolog, int il_len) {
 }
 
 /* Generate shader from file path - general function */
-unsigned int _gl_genshader(const char* path, int type, char* charbuf, int charbufsz) {
+unsigned int f_gl_genshader(const char* path, int type, char* charbuf, int charbufsz) {
 	unsigned int s = glCreateShader(type);
 	int len = 0;
 	_io_filetobuf(path, &len, charbuf, charbufsz);
 	glShaderSource(s, 1, (const char* const*)(&charbuf), NULL);;
 	memset(charbuf, 0, len+1);
 	glCompileShader(s);
-	_gl_chkcmp(s, charbuf, charbufsz);
+	f_gl_chkcmp(s, charbuf, charbufsz);
 	return s;
 }
 
 /* Check if program was linked successfully */
-void _gl_chklink(unsigned int sp, char *infolog, int il_len) {
+void f_gl_chklink(unsigned int sp, char *infolog, int il_len) {
 	int success = 0;
 	glGetProgramiv(sp, GL_LINK_STATUS, &success);
 	if(success) return;
@@ -239,7 +239,7 @@ void _gl_chklink(unsigned int sp, char *infolog, int il_len) {
 }
 
 /* Get all shaders attached to a program, detach them and delete them */
-void _gl_detachprogshaders(unsigned int sp) {
+void f_gl_detachprogshaders(unsigned int sp) {
 	int n = 0;
 	while(glGetProgramiv(sp, GL_ATTACHED_SHADERS, &n), n) {
 		unsigned int s;
@@ -250,7 +250,7 @@ void _gl_detachprogshaders(unsigned int sp) {
 
 /* Generate shader program - with va_arg */
 /* Arguments are terminated by 0 */
-unsigned int _gl_genprogram_v(char* infolog, int il_len, va_list args) {
+unsigned int f_gl_genprogram_v(char* infolog, int il_len, va_list args) {
 	unsigned int sp = glCreateProgram();
 	unsigned int s = va_arg(args, unsigned int);
 	while(s) {
@@ -258,31 +258,31 @@ unsigned int _gl_genprogram_v(char* infolog, int il_len, va_list args) {
 		s = va_arg(args, unsigned int);
 	}
 	glLinkProgram(sp);
-	_gl_chklink(sp, infolog, il_len);
+	f_gl_chklink(sp, infolog, il_len);
 	return sp;
 }
 
 /* Generate shader program - with variadic inputs */
 /* Arguments are terminated by 0 */
-unsigned int _gl_genprogram(char* infolog, int il_len, ...) {
+unsigned int f_gl_genprogram(char* infolog, int il_len, ...) {
 	va_list args;
 	va_start(args, il_len);
-	unsigned int sp = _gl_genprogram_v(infolog, il_len, args);
+	unsigned int sp = f_gl_genprogram_v(infolog, il_len, args);
 	va_end(args);
 	return sp;
 }
 
 /* Wrapper macro adding terminating 0 */
-#define _gl_GenProgram(...) _gl_genprogram(g_charbuf, CHBUFSZ_, __VA_ARGS__ __VA_OPT__(,) 0)
-#define _gl_GenShader(path, type) _gl_genshader(path, type, g_charbuf, CHBUFSZ_);
+#define f_gl_GenProgram(...) f_gl_genprogram(g_charbuf, CHBUFSZ_, __VA_ARGS__ __VA_OPT__(,) 0)
+#define f_gl_GenShader(path, type) f_gl_genshader(path, type, g_charbuf, CHBUFSZ_);
 
 void updatetime(double *time, double *t0, double *dt) {
 	*time = glfwGetTime(), *dt = *time - *t0, *t0 = *time;
 }
 
-enum proghandlemethod { PROG_GEN, PROG_CR, PROG_DEL, PROG_USE_1, PROG_USE_2 };
+enum e_proghandlemethod { PROG_GEN, PROG_CR, PROG_DEL, PROG_USE_1, PROG_USE_2 };
 
-void proghandler(enum proghandlemethod method) {
+void proghandler(enum e_proghandlemethod method) {
 	static unsigned int vert = 0, geom = 0, frag = 0;
 	static unsigned int sp1 = 0, sp2 = 0;
 	static int time_loc = -1;
@@ -307,15 +307,15 @@ void proghandler(enum proghandlemethod method) {
 		create:
 		case PROG_CR:
 			/* Shaders */
-			vert = _gl_GenShader("vertex.glsl", GL_VERTEX_SHADER);
-			geom = _gl_GenShader("geom.glsl", GL_GEOMETRY_SHADER);
-			frag = _gl_GenShader("fragment.glsl", GL_FRAGMENT_SHADER);
+			vert = f_gl_GenShader("vertex.glsl", GL_VERTEX_SHADER);
+			geom = f_gl_GenShader("geom.glsl", GL_GEOMETRY_SHADER);
+			frag = f_gl_GenShader("fragment.glsl", GL_FRAGMENT_SHADER);
 			/* Shader Programs */
-			sp1 = _gl_GenProgram(vert, frag);
-			sp2 = _gl_GenProgram(vert, geom, frag);
+			sp1 = f_gl_GenProgram(vert, frag);
+			sp2 = f_gl_GenProgram(vert, geom, frag);
 
-			_gl_detachprogshaders(sp1);
-			_gl_detachprogshaders(sp2);
+			f_gl_detachprogshaders(sp1);
+			f_gl_detachprogshaders(sp2);
 			/* Uniform location */
 			time_loc = glGetUniformLocation(sp2, "time");
 			if(time_loc < 0) fprintf(stderr, "ERROR: Unable to get uniform location!\n");
@@ -323,14 +323,14 @@ void proghandler(enum proghandlemethod method) {
 }
 
 /* Evaluate keyboard and mouse events - currently handles shortcuts for hot reloading and exit */
-void evalqueue(struct _glfw_inputqueue *q) {
+void evalqueue(struct t_glfw_inputqueue *q) {
 	for(int i = q->start; i != q->end; ++i) {
 		if(q->queue[i].key == GLFW_KEY_R && q->queue[i].mods == GLFW_MOD_CONTROL)
 			proghandler(PROG_GEN);
 		if(q->queue[i].key == GLFW_KEY_Q && q->queue[i].mods == (GLFW_MOD_CONTROL | GLFW_MOD_SHIFT) )
 			ws.runstate = 0;
 	}
-	_iqclear(q);
+	f_iqclear(q);
 }
 
 void rotate2df(float pos[2], float out[2], double angle) {
@@ -350,7 +350,7 @@ float vertices[] = {
 
 /* Main function */
 int main(void) {
-	_glfw_initialize(&ws, "Hexagon");
+	f_glfw_initialize(&ws, "Hexagon");
 	gladLoadGL(glfwGetProcAddress);
 
 	proghandler(PROG_GEN);
