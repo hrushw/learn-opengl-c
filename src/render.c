6b777a9e3592c33430d44a6807e45ed1d3dcc569
Hrusheekesh Warnekar <192113352+hrushw@users.noreply.github.com>
2025-08-09 19:04:04 +0530

    Revert "split matrix transformation functions into separate file"
    This reverts commit d06af5156852d7b850bd1f29628ec7e2ef033b6e.

--- src/render.c
+++ src/render.c
@@ -6,7 +6,6 @@
 #include <math.h>
 
 #include "window.h"
-#include "vector.c"
 
 /* References
  * OpenGL Tutorials (Victor Gordan) - https://www.youtube.com/playlist?list=PLPaoO-vpZnumdcb4tZc4x5Q-v7CkrQ6M-
@@ -146,6 +145,130 @@ void f_render_evalstate(struct t_glfw_winstate *wst) {
 	wst->iq.start = 0, wst->iq.end = 0;
 }
 
+
+/* ---------------------- *
+ * Matrix transformations *
+ * ---------------------- */
+
+struct mat4x4f {
+	float arr[4][4];
+};
+
+const struct mat4x4f c_mat4x4f_identity = {{
+	{ 1.0, 0.0, 0.0, 0.0 },
+	{ 0.0, 1.0, 0.0, 0.0 },
+	{ 0.0, 0.0, 1.0, 0.0 },
+	{ 0.0, 0.0, 0.0, 1.0 },
+}};
+
+const struct mat4x4f c_mat4x4f_zero = {{
+	{ 0.0, 0.0, 0.0, 0.0 },
+	{ 0.0, 0.0, 0.0, 0.0 },
+	{ 0.0, 0.0, 0.0, 0.0 },
+	{ 0.0, 0.0, 0.0, 0.0 },
+}};
+
+struct mat4x4f f_mat_multiply(struct mat4x4f a, struct mat4x4f b) {
+	struct mat4x4f out = c_mat4x4f_zero;
+
+	for(int i = 0; i < 4; ++i)
+		for(int j = 0; j < 4; ++j)
+			for(int k = 0; k < 4; ++k)
+				out.arr[i][j] += a.arr[i][k] * b.arr[k][j];
+
+	return out;
+}
+
+struct mat4x4f f_mat_multiplylist(struct mat4x4f *m, int n) {
+	if(n == 0)
+		return c_mat4x4f_identity;
+
+	struct mat4x4f out = m[0];
+	for(int i = 1; i < n; ++i)
+		out = f_mat_multiply(out, m[i]);
+	return out;
+}
+
+struct mat4x4f f_mat_scale3d(float x, float y, float z) {
+	return (struct mat4x4f){{
+		{   x, 0.0, 0.0, 0.0 },
+		{ 0.0,   y, 0.0, 0.0 },
+		{ 0.0, 0.0,   z, 0.0 },
+		{ 0.0, 0.0, 0.0, 1.0 },
+	}};
+}
+
+static inline struct mat4x4f f_mat_scale(float s) {
+	return f_mat_scale3d(s, s, s);
+}
+
+struct mat4x4f f_mat_translate(float x, float y, float z) {
+	return (struct mat4x4f){{
+		{ 1.0, 0.0, 0.0,   x },
+		{ 0.0, 1.0, 0.0,   y },
+		{ 0.0, 0.0, 1.0,   z },
+		{ 0.0, 0.0, 0.0, 1.0 },
+	}};
+}
+
+struct mat4x4f f_mat_rotatex(double angle) {
+	const float c = cos(angle);
+	const float s = sin(angle);
+
+	return (struct mat4x4f) {{
+		{ 1.0, 0.0, 0.0, 0.0 },
+		{ 0.0,   c,  -s, 0.0 },
+		{ 0.0,   s,   c, 0.0 },
+		{ 0.0, 0.0, 0.0, 1.0 },
+	}};
+}
+
+struct mat4x4f f_mat_rotatey(double angle) {
+	const float c = cos(angle);
+	const float s = sin(angle);
+
+	return (struct mat4x4f) {{
+		{   c, 0.0,   s, 0.0 },
+		{ 0.0, 1.0, 0.0, 0.0 },
+		{  -s, 0.0,   c, 0.0 },
+		{ 0.0, 0.0, 0.0, 1.0 },
+	}};
+}
+
+struct mat4x4f f_mat_rotatez(double angle) {
+	const float c = cos(angle);
+	const float s = sin(angle);
+	return (struct mat4x4f) {{
+		{   c,  -s, 0.0, 0.0 },
+		{   s,   c, 0.0, 0.0 },
+		{ 0.0, 0.0, 1.0, 0.0 },
+		{ 0.0, 0.0, 0.0, 1.0 },
+	}};
+}
+
+struct mat4x4f f_mat_perspective(double fov, double near, double far) {
+	const double zscale = (near + far)/(far - near);
+	const double ztrans = 2*near*far/(near - far);
+	const float sp = 1.0/tanf(fov/2.0);
+
+	return (struct mat4x4f) {{
+		{  sp, 0.0,    0.0,    0.0 },
+		{ 0.0,  sp,    0.0,    0.0 },
+		{ 0.0, 0.0, zscale, ztrans },
+		{ 0.0, 0.0,    1.0,    0.0 },
+	}};
+}
+
+enum e_transform_type {
+	MAT_TRANSLATE,
+	MAT_PERSPECTIVE,
+	MAT_SCALE3D,
+	MAT_SCALE,
+	MAT_ROTATEX,
+	MAT_ROTATEY,
+	MAT_ROTATEZ,
+};
+
 /* xyz coordinates, rgb colors, texture coordinates  */
 const float vertices[] = {
 	 0.0f,  0.8f,  0.0f,    1.0f, 1.0f, 1.0f,    0.0f, 0.0f,
@@ -200,10 +323,21 @@ void f_render_init(void) {
 void f_render_loop(void* win, int transformloc) {
 	struct t_glfw_winstate* const wst = glfwGetWindowUserPointer(win);
 
+	/* Projection matrix must be applied last - *
+	 * fixed function stage in OpenGL scales all vectors down by w */
+	struct mat4x4f transforms[] = {
+		c_mat4x4f_identity,
+		f_mat_perspective(M_PI/3.0, 1.0, 6.0),
+		f_mat_translate(0.6f, 0.0f, 2.0f),
+		c_mat4x4f_identity,
+		f_mat_translate(0.4f, 0.1f, 0.0f),
+		f_mat_scale(0.5f),
+		c_mat4x4f_identity,
+		c_mat4x4f_identity,
+	};
+
 	/* Initialize time and loop */
 	for(glfwSetTime(0.0); wst->runstate; wst->time = glfwGetTime()) {
-		static struct mat4x4f tf = c_mat4x4f_identity;
-
 		/* Set viewport and view transform matrix */
 		if(wst->szrefresh) {
 			wst->szrefresh = 0;
@@ -215,8 +349,14 @@ void f_render_loop(void* win, int transformloc) {
 			else
 				scalex = 1.0, scaley = (double)wst->height / (double)wst->width;
 
-			tf = f_mat_scale3d(1.0/scalex, 1.0/scaley, 1.0);
+			transforms[0] = f_mat_scale3d(1.0/scalex, 1.0/scaley, 1.0);
 		}
+
+		transforms[3] = f_mat_rotatez(wst->time);
+		transforms[6] = f_mat_rotatez(-wst->time);
+		transforms[7] = f_mat_rotatey(3*wst->time);
+
+		const struct mat4x4f tf = f_mat_multiplylist(transforms, M_LEN(transforms));
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glUniformMatrix4fv(transformloc, 1, GL_TRUE, &(tf.arr[0][0]));
