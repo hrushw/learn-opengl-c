e5410f543cf03a83e4e9136c32de0ae6c3b3870b
Hrusheekesh Warnekar <192113352+hrushw@users.noreply.github.com>
2025-08-27 06:09:46 +0530

    testing program compilation, fixed accidental stack smashing error

--- src/render.c
+++ src/render.c
@@ -8,7 +8,7 @@
 #include "fileio.h"
 #include "shader.h"
 
-void f_log_f2b(enum e_filetobuf_error ret, const char* path, unsigned int buflen, unsigned int flen) {
+void f_error_log_f2b(enum e_filetobuf_error ret, const char* path, unsigned int buflen, unsigned int flen) {
 	switch(ret & ~ERR_F2B_FAILED_CLOSE) {
 	case ERR_F2B_ZERO_SIZE_BUFFER:
 		fprintf(stderr, "ERROR: Cannot read into zero sized buffer!\n");
@@ -50,7 +50,7 @@ void f_log_f2b(enum e_filetobuf_error ret, const char* path, unsigned int buflen
 		fprintf(stderr, "ERROR: Failed to close file '%s'\n", path);
 }
 
-void f_log_shader(enum e_gl_shader_check ret, int type, char* logbuf, unsigned int loglen) {
+void f_error_log_shader(enum e_gl_shader_check ret, int type, char* logbuf, unsigned int loglen) {
 	const char* typestr;
 	switch(type) {
 	case GL_VERTEX_SHADER:
@@ -87,6 +87,29 @@ void f_log_shader(enum e_gl_shader_check ret, int type, char* logbuf, unsigned i
 	}
 }
 
+void f_error_log_program(enum e_gl_shader_check ret, char* logbuf, unsigned int loglen) {
+	switch(ret) {
+	case ERR_GL_SHADER_FAIL:
+		fprintf(stderr, "ERROR: Failed to link program!\n[ERROR LOG]:\n%s\n", logbuf);
+		break;
+
+	case ERR_GL_SHADER_FAIL_LOG_INCOMPLETE:
+		fprintf(stderr,
+			"ERROR: Failed to link program!\n"
+			"[ERROR LOG][INCOMPLETE, full size = %d bytes]:\n%s\n",
+			loglen, logbuf
+		);
+		break;
+
+	case ERR_GL_SHADER_SUCCESS:
+		fprintf(stderr, "[LOG]: Successfully linked program!\n");
+		break;
+
+	default:
+		fprintf(stderr, "ERROR: Invalid return from function 'f_gl_genprogram'!\n");
+	}
+}
+
 void f_render_main(void* win) {
 	enum e_bufsz_src { BUFSZ_SRC = 0x2000 };
 	enum e_bufsz_log { BUFSZ_LOG = 0x1000 };
@@ -97,7 +120,7 @@ void f_render_main(void* win) {
 	char* frag_src = chbuf + BUFSZ_SRC;
 	char* vert_log = chbuf + 2*BUFSZ_SRC;
 	char* frag_log = chbuf + 2*BUFSZ_SRC + BUFSZ_LOG;
-	char* prog_log = chbuf + 2*BUFSZ_SRC + 3*BUFSZ_LOG;
+	char* prog_log = chbuf + 2*BUFSZ_SRC + 2*BUFSZ_LOG;
 
 	unsigned int len = 0;
 	const char* path;
@@ -105,11 +128,23 @@ void f_render_main(void* win) {
 
 	path = "shaders/vertex.glsl";
 	ret = f_io_filetobuf(path, &len, vert_src, BUFSZ_SRC);
-	f_log_f2b(ret, path, BUFSZ_SRC, len);
+	f_error_log_f2b(ret, path, BUFSZ_SRC, len);
 
 	unsigned int vert = 0;
 	ret = f_gl_genshader(&vert, GL_VERTEX_SHADER, vert_src, vert_log, BUFSZ_LOG, &len);
-	f_log_shader(ret, GL_VERTEX_SHADER, vert_log, len);
+	f_error_log_shader(ret, GL_VERTEX_SHADER, vert_log, len);
+
+	path = "shaders/fragment.glsl";
+	ret = f_io_filetobuf(path, &len, frag_src, BUFSZ_SRC);
+	f_error_log_f2b(ret, path, BUFSZ_SRC, len);
+
+	unsigned int frag = 0;
+	ret = f_gl_genshader(&frag, GL_FRAGMENT_SHADER, frag_src, frag_log, BUFSZ_LOG, &len);
+	f_error_log_shader(ret, GL_FRAGMENT_SHADER, frag_log, len);
+
+	unsigned int sp = 0;
+	ret = f_gl_genprogram(&sp, vert, frag, prog_log, BUFSZ_LOG, &len);
+	f_error_log_program(ret, prog_log, len);
 
 	struct t_glfw_winstate* wst = glfwGetWindowUserPointer(win);
 
